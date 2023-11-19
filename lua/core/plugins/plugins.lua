7ca229b62bd3a74bdaa810e10e577c74e7fa7085
Michael <michaelnunezt@hotmail.com>
2023-11-19 11:17:57 -0500

    update

--- lua/core/plugins/plugins.lua
+++ lua/core/plugins/plugins.lua
@@ -54,6 +54,7 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
+      'nvim-telescope/telescope-symbols.nvim',
       'nvim-lua/plenary.nvim',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
