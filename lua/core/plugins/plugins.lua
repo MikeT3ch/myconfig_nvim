9870a4b52ff6780a22331c817e687305eb01e19b
Michael <michaelnunezt@hotmail.com>
2023-11-19 14:47:43 -0500

    pynappo files in core, utils and theme lua.

--- lua/core/plugins/plugins.lua
+++ lua/core/plugins/plugins.lua
@@ -26,16 +26,69 @@ return {
     },
     build = ':TSUpdate',
   },
+
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    branch = 'v3',
+    event = 'BufReadPre',
+    config = function()
+      local hl_name_list = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterCyan',
+        'RainbowDelimiterViolet',
+      }
+      require('ibl').setup({
+        scope = {
+          enabled = true,
+          show_start = false,
+          highlight = hl_name_list
+        }
+      })
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end,
   },
+
+  {
+    'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+    config = function()
+      local colors = {
+        Red = '#EF6D6D',
+        Orange = '#FFA645',
+        Yellow = '#EDEF56',
+        Green = '#6AEF6F',
+        Cyan = '#78E6EF',
+        Blue = '#70A4FF',
+        Violet = '#BDB2EF',
+      }
+      require('core.themepy').set_rainbow_colors('RainbowDelimiter', colors) -- just a helper function that sets the highlights with the given prefix
+      local rainbow_delimiters = require('rainbow-delimiters')
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterViolet',
+        },
+      }
+    end
+  },
+
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
---
+++ lua/core/themepy.lua
@@ -0,0 +1,91 @@
+local theme = {}
+
+local OverrideTable = {}
+
+function OverrideTable:new()
+	local object = {
+		vim_highlights = setmetatable({}, {
+			__newindex = function(t, k, v)
+				vim.cmd.highlight(k .. ' ' .. v)
+				rawset(t, k, v)
+			end,
+		}),
+		nvim_highlights = setmetatable({}, {
+			__newindex = function(t, k, v)
+				vim.api.nvim_set_hl(0, k, v)
+				rawset(t, k, v)
+			end,
+		}),
+		links = setmetatable({}, {
+			__newindex = function(t, k, v)
+				vim.cmd('highlight! link ' .. k .. ' ' .. v)
+				rawset(t, k, v)
+			end,
+		}),
+		functions = setmetatable({}, {
+			__newindex = function(t, k, v)
+				v()
+				rawset(t, k, v)
+			end,
+		}),
+	}
+
+	setmetatable(object, self)
+	self.__index = self
+	return object
+end
+
+function OverrideTable.apply(self)
+	for _, table in pairs(self) do
+		if type(table) == 'table' then
+			local newindex_func = getmetatable(table).__newindex
+			for key, value in pairs(table) do
+				newindex_func(table, key, value)
+			end
+		end
+	end
+end
+
+theme.overrides = {
+	all = OverrideTable:new(),
+}
+setmetatable(theme.overrides, {
+	__index = function(t, k)
+		rawset(t, k, OverrideTable:new())
+		return t[k]
+	end,
+})
+
+local transparent_highlights = {
+	'Normal',
+	'NormalNC',
+	'LineNr',
+	'Folded',
+	'NonText',
+	'SpecialKey',
+	'VertSplit',
+	'SignColumn',
+	'EndOfBuffer',
+	'TablineFill',
+}
+
+if not require('core.utilspy').is_gui then
+	for _, hl in ipairs(transparent_highlights) do
+		theme.overrides.all.vim_highlights[hl] = 'guibg=NONE ctermbg=NONE'
+	end
+end
+
+function theme.set_rainbow_colors(prefix, colors)
+	local hl_names = {}
+	for _, color in ipairs(colors) do
+		local name = color[1]
+		local hex = color[2]
+		local hl_name = prefix .. name
+		table.insert(hl_names, hl_name)
+		vim.api.nvim_set_hl(0, hl_name, { fg = hex })
+		theme.overrides.all.nvim_highlights[hl_name] = { fg = hex, nocombine = true }
+	end
+	return hl_names
+end
+
+return theme
