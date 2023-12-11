5038567c8cc136c46aea806f5180107dc025b5fa
MikeT3ch <michaelnunezt@hotmail.com>
2023-12-11 10:49:09 -0500

    termux plugins

---
+++ lua/core/plugins/termux.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "sudoerwx/vim-ray-so-beautiful"
+  },
+  {
+    "kristijanhusak/vim-carbon-now-sh"
+  },
+  {
+      "HakonHarnes/img-clip.nvim",
+      cmd = "PasteImage",
+      opts = {
+      --         keys = {
+      --             -- suggested keymap
+      --                 { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+      --                   },
+    }
+  }
+}
