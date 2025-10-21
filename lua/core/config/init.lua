013e6b263164e9a3c0f9e8ebb6f1b7b50ef31d7c
MikeT3ch <124947421+MikeT3ch@users.noreply.github.com>
2025-10-21 15:29:01 -0500

    Changes to mason and other lsp config

--- lua/core/config/init.lua
+++ lua/core/config/init.lua
@@ -1,21 +1,21 @@
 -- require('core.config.java_configs')
 
+require('core.config.theme')
+
+require('core.config.LSP_configs')
+
 require('core.config.LSP_configs')
 
 require('core.config.options')
 
+require('core.config.nvim-cmp_configs')
+
 require('core.config.keymaps')
 
-require('core.config.theme')
-
 require('core.config.plugins_configs')
-
-require('core.config.nvim-cmp_configs')
 
 require('core.config.treesitter_configs')
 
 require('core.config.which-key_configs')
-
-require('core.config.LSP_configs')
 
 require('core.config.ufo_config')
